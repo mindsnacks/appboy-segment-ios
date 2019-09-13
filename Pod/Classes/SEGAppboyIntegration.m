61c23fa552c8557d23fe4e94c0ce168336d44ead
Jeremy Levine <jeremy.levine@mindsnacks.com>
2019-09-13 13:09:16 -0700

    Refactor to keep the customEndpoint key

--- Pod/Classes/SEGAppboyIntegration.m
+++ Pod/Classes/SEGAppboyIntegration.m
@@ -35,7 +35,12 @@
     }
 
     NSMutableDictionary *appboyOptions = [@{ABKSDKFlavorKey : @(SEGMENT),
-                                            ABKEndpointKey: @"sdk.iad-01.braze.com"} mutableCopy];
+                                            ABKEndpointKey: @"sdk.iad-01.braze.com"
+                                            } mutableCopy];
+    NSString *customEndpoint = self.settings[@"customEndpoint"];
+    if (customEndpoint && [customEndpoint length] != 0) {
+      appboyOptions[ABKEndpointKey] = customEndpoint;
+    }
 
     if ([NSThread isMainThread]) {
       [Appboy startWithApiKey:appboyAPIKey
